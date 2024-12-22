04d67a63b07a2565d8a8e024e75694bc242978f2
Mustak24 <162583970+Mustak24@users.noreply.github.com>
2024-12-22 12:59:25 +0530

    Add Netlify plugin for Next.js; update database connection logic with new MongoDB URL; create Netlify configuration file

--- pages/api/Middlewares/connectToDb.js
+++ pages/api/Middlewares/connectToDb.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 export default async function connetToDb(req, res, next){
     if(mongoose.connections[0].readyState) return next(req, res);
     try{
-        await mongoose.connect(process.env.MONGO_URL);
+        let url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@notes-with-bot.ff84r.mongodb.net/hostel`;
+        await mongoose.connect(url);
         return next(req, res);
     } catch(error){
         res.status(500).json({alert: {type: 'error', msg: 'Fail to connect to database'}, error, miss: false});
