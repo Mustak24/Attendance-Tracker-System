6d5129bf2f0e3b3d9e47dee712f7bdba73235e40
TOHID KHAN <162583970+Mustak24@users.noreply.github.com>
2024-12-25 22:10:19 +0530

    Update database connection logic to use environment variables for MongoDB URL

--- pages/api/Middlewares/connectToDb.js
+++ pages/api/Middlewares/connectToDb.js
@@ -4,7 +4,7 @@ export default async function connetToDb(req, res, next){
     if(mongoose.connections[0].readyState) return next(req, res);
     try{
         let url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@notes-with-bot.ff84r.mongodb.net/hostel`;
-        await mongoose.connect('mongodb://localhost:27017/hostel');
+        await mongoose.connect(url);
         return next(req, res);
     } catch(error){
         res.status(500).json({alert: {type: 'error', msg: 'Fail to connect to database'}, error, miss: false});
